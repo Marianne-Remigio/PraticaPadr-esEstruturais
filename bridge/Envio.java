b2d0f04cf30c9b1439151f5f4f1c945a621990e3
Kaio <kaio.bezerra1809@gmail.com>
2025-11-20 20:02:17 -0300

    Refactor Envio interface and EnvioEmail class

--- bridge/Envio.java
+++ bridge/Envio.java
@@ -4,11 +4,11 @@ public interface Envio {
     void enviar();
 }
 
-// Implementação concreta para Email
 public class EnvioEmail implements Envio {
     @Override
     public void enviar() {
         System.out.println("Enviando por EMAIL...");
     }
 }
+
 
