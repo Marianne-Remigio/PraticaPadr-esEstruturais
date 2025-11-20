edb83f10b33f67c0b38f649e12789bebad9b6bcc
Kaio <kaio.bezerra1809@gmail.com>
2025-11-20 19:58:42 -0300

    Create Aplicativo.java

---
+++ bridge/Aplicativo.java
@@ -0,0 +1,16 @@
+package org.example.bridge;
+
+public class Aplicativo {
+    public static void main(String[] args) {
+        // Combinando abstrações com implementações livremente
+        NotaFiscal nota1 = new NotaFiscalEmail(new EnvioEmail());
+        nota1.emitir("Maria José", 1200.00);
+
+        NotaFiscal nota2 = new NotaFiscalImpressora(new EnvioImpressora());
+        nota2.emitir("João José", 942.00);
+
+        // Agora podemos misturar - nota de email sendo impressa, por exemplo
+        NotaFiscal nota3 = new NotaFiscalEmail(new EnvioImpressora());
+        nota3.emitir("Pedro Silva", 750.00);
+    }
+}
